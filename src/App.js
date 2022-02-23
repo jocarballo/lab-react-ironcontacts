e92212e19ceadefa91d7261c0590030483f902c7
Joana Carballo <joana16cruz@gmail.com>
2022-02-23 12:29:30 +0100

    4 iteration not finished

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 function App() {
 
-  let [contactsList, setContactsList] = useState(contacts.splice(0, 5))
+  let [contactsList, setContactsList] = useState(contacts.splice(0, 10))
 
   function RandomContact(celebrities) {
     let random = celebrities[Math.floor(Math.random()*celebrities.length)];
@@ -16,11 +16,28 @@ function App() {
     setContactsList(newArray);
   }
   
+  //let sortByName = () => {
+    //return contact1.name - contact2.name
+  //};
+
+  
+  let sortByName = contactsList.sort( (a, b) => a.localeCompare(b, 'fr'))
+
+  let sortByPopularity = contactsList.sort( (a, b) => a.localeCompare(b, 'fr'))
+  
+  //let sortByPopularity = () => {
+    //return contact2.popularity - contact1.popularity
+  //};
+
+
+  console.log("Contacts",)
 
   return (
   <div className="App">
   <h1>Iron Contacts</h1>
     <button onClick={() => RandomContact(contacts)}>Add Random Contact</button>
+    <button onClick={() => contactsList(sortByName)}>Sort by Name</button>
+    <button onClick={() => contactsList(sortByPopularity)}>Sort by Popularity</button>
 
     <div className="contactsTable">
       <table>
